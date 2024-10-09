19c47760bdb573776449c7f83d31600c72902693
sojha <soumya.jha@nokia.comc>
2024-10-09 22:53:43 +0530

    using pytest exception kywrd

--- asf/test.py
+++ asf/test.py
@@ -1,3 +1,4 @@
+import pytest
 def fuzzy_math(num1, operator, num2):
     if type(num1) != int or type(num2) != int:
         raise Exception('We need to do fuzzy maths on ints')
@@ -20,26 +21,49 @@ def fuzzy_math(num1, operator, num2):
 
 class TestFuzzyMath:
     def test_non_int_input_for_num1(self):
-        pass
+        with pytest.raises(Exception) as exc_info:
+            fuzzy_math('hi', '+', 34)
+        assert 'fuzzy maths on ints' in str(exc_info)
     def test_non_int_input_for_num2(self):
-        pass
+        '''
+        error1 = None
+        try:
+            fuzzy_math(2, '+', 23.2)
+        except Exception as e1:
+            error1 = e1
+        assert error1 is not None
+        '''
+        with pytest.raises(Exception) as exec_info:
+            fuzzy_math(3,'+', 34.2)
+        assert 'fuzzy maths on ints' in str(exec_info)
     def test_addition_with_negative_result(self):
-        pass
+        assert fuzzy_math(-1, '+', -4) == 'A negative number, what does that even mean?'
     def test_addition_with_small_result(self):
         assert fuzzy_math(2, '+', 2) == 'A small number, I can deal with that.' #asserting that output of fuzzy math is equal to the number(on left).
     def test_addition_with_medium_result(self):
-        pass
+        assert fuzzy_math(12, '+', 4) == 'A medium-sized number. OK.'
     def test_addition_with_large_result(self):
-        pass
+        assert fuzzy_math(45, '+', 34) == 'A really big number, way too big for me.'
     def test_with_invalid_operator(self):
-        pass
+        '''
+        error2 = None
+        try:
+            fuzzy_math(32, '/', 2)
+        except Exception as e2:
+            error2 = e2
+        assert error2 is not None
+        :return:
+        '''
+        with pytest.raises(Exception) as exec_info:
+            fuzzy_math(21, '/', 24)
+        assert "I don't know how to do math" in str(exec_info)
     def test_multiplication_with_negative_result(self):
-        pass
+        assert fuzzy_math(-3, '*', 6) == 'A negative number, what does that even mean?'
     def test_multiplication_with_small_result(self):
-        pass
+        assert fuzzy_math(3, '*', 1) == 'A small number, I can deal with that.'
     def test_multiplication_with_medium_result(self):
-        pass
+        assert fuzzy_math(4, '*', 3) == 'A medium-sized number. OK.'
     def test_multiplication_with_large_result(self):
-        pass
+        assert fuzzy_math(9, '*', 8) == 'A really big number, way too big for me.'
 
 
